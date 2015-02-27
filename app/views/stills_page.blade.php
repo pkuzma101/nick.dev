7c8ed53bec09cee5f0f10caa9fb301e90f98fd8d
pkuzma101 <kuzma.paul@gmail.com>
2015-02-27 16:30:36 -0500

    Going to try a magnetic popup plugin, committing incase it doesn't work

--- app/views/stills_page.blade.php
+++ app/views/stills_page.blade.php
@@ -11,15 +11,20 @@
 				<li><img src="/stills_folder/Rott_High_0022.jpg" class="slide-image center-block"></li>
 				<li><img src="/stills_folder/jet_blue2.jpg" class="slide-image center-block"></li>
 				<li><img src="/stills_folder/samples_honeybee2.jpg" class="slide-image center-block"></li>
+				<li><img src="/stills_folder/jumpball_bubble2.jpg" class="slide-image center-block"></li>
+				<li><img src="/stills_folder/jumpball_feather.jpg" class="slide-image center-block"></li>
 			</ul>
-			<div id="bx-pager">
-				<a data-slide-index="0" href""><img src="/stills_folder/Cheetah_Speedform.jpg" class="slide-thumbnail"></a>
-				<a data-slide-index="1" href""><img src="/stills_folder/jet_black.jpg" class="slide-thumbnail"></a>
-				<a data-slide-index="2" href""><img src="/stills_folder/Rott_High_002.jpg" class="slide-thumbnail"></a>
-				<a data-slide-index="3" href""><img src="/stills_folder/jet_blue.jpg" class="slide-thumbnail"></a>
-				<a data-slide-index="4" href""><img src="/stills_folder/samples_honeybee.jpg" class="slide-thumbnail"></a>
-			</div>
 		</div>
+			<div id="bx-pager">
+				<a data-slide-index="0" href""><img src="/stills_folder/Cheetah_Speedform2.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="1" href""><img src="/stills_folder/jet_black2.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="2" href""><img src="/stills_folder/Rott_High_0022.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="3" href""><img src="/stills_folder/jet_blue2.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="4" href""><img src="/stills_folder/samples_honeybee2.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="5" href""><img src="/stills_folder/jumpball_bubble2.jpg" class="slide-thumbnail"></a>
+				<a data-slide-index="6" href""><img src="/stills_folder/jumpball_feather.jpg" class="slide-thumbnail"></a>
+			</div>
+		
 	</section>
 </div>
 
